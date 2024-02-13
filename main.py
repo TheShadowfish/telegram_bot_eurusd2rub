32d2937f172fd418730212b4236f4d10f8abd0b3
Эдуард Кусков <shadowfish@yandex.ru>
2024-02-14 00:01:32 +0300

    Вроде как прикрутили к боту в telegram

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from decouple import config
 # from dotenv import load_dotenv
 import requests
 
-
 import os
 
 from src.apilayer_currency import currency_now
@@ -13,62 +12,71 @@ from src.apilayer_currency import currency_list
 from src.apilayer_currency import save_to_json_cash
 from src.apilayer_currency import read_json_cash
 
-import telebot;
-bot = telebot.TeleBot('%ваш токен%');
-# from src.apilayer_currency import currency_historical
+# import telebot
+#
+# bot = telebot.TeleBot(config('TG_BOT_API_KEY'), parse_mode=None)  # You can set parse_mode by default. HTML or MARKDOWN
+# bot.polling(none_stop=True, interval=0)
+#
+#
+# # @bot.message_handler(content_types=['text'])
+# # def get_text_messages(message):
+# #     if message.text == "Привет":
+# #         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+# #     elif message.text == "/help":
+# #         bot.send_message(message.from_user.id, "Напиши привет")
+# #     else:
+# #         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+# #     print(f"I do...")
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+#     bot.reply_to(message, "Howdy, how are you doing?")
+#
+#
+# @bot.message_handler(func=lambda m: True)
+# def echo_all(message):
+#     bot.reply_to(message, message.text)
 
-
-def main2():
-    pass
-    # api_key = 'nCgAhGbEvs14xb15z2GJdVc0PsLvk1eq'
-    # date = '2021-02-28'
-    # print(currency_now(api_key))
-    # print(f"\n")
-    # print(currency_historical(api_key, date))
-
-def telebot_in_work():
-    while True:
-        # print(f"Получить данные с сайта и записать в файл 'rw'")
-        # print(f"Получить данные с сайта, в файл не писать 'r'")
-        # print(f"Получить список валют с сайта 'l'")
-        # print(f"Получить данные или из файла 'f'")
-        print(f"Работа с ботом telegram 't'")
-        print(f"Выход 'q'")
-
-        parameter = input('Введите команду: ')
-        if parameter == 't':
-
-            # data = currency_now(api_key, 'EUR', 'RUB')
-        # elif parameter == 'rw':
-        #     data = currency_now(api_key, 'EUR', 'RUB')
-        #
-        #     if (data is not None):
-        #         save_to_json_cash(data)
-        #     else:
-        #         print(f"Данные с сайта получить не удалось")
-        # elif parameter == 'f':
-        #     data = read_json_cash()
-        elif parameter == 'q':
-            break
-        # elif parameter == 'l':
-        #     data = currency_list(api_key)
-        # elif parameter == 't':
-        #     data = currency_list(api_key)
-        else:
-            print(f"неверный параметр")
-
-        for key, value in data.items():
-            if type(value) is dict:
-                print(f"{key}:")
-                for key, value in value.items():
-                    print(f"  {key}: {value}")
-            else:
-                print(key, value, sep=': ')
+#
+# def telebot_in_work(parameter):
+#     api_key = config('TG_BOT_API_KEY')
+#     print(api_key)
+#
+#     api_key = config('APILAYER_APIKEY')
+#     print(api_key)
+#
+#     data: dict = {}
+#
+#     if parameter == 'apilayer':
+#         data = currency_now(api_key, 'EUR', 'RUB')
+#     elif parameter == 'apilayer_to_file':
+#         data = currency_now(api_key, 'EUR', 'RUB')
+#
+#         if data is not None:
+#             save_to_json_cash(data)
+#         else:
+#             print(f"Данные с сайта получить не удалось")
+#     elif parameter == 'file':
+#         data = read_json_cash()
+#     elif parameter == 'list':
+#         data = currency_list(api_key)
+#     else:
+#         print(f"неверный параметр")
+#
+#     data_in_bot = ''
+#
+#     for key, value in data.items():
+#         if type(value) is dict:
+#             data_in_bot += f'{key}:\n'
+#             for key2, value2 in value.items():
+#                 data_in_bot += f"  {key2}: {value2}"
+#         else:
+#             data_in_bot += f'{key}:  {value}'
+#     return data_in_bot
 
 
 def main():
     # load_dotenv()
-    #database_url = config('DATABASE_URL')
+    # database_url = config('DATABASE_URL')
     api_key = config('APILAYER_APIKEY')
     print(api_key)
     data: dict = {}
@@ -86,7 +94,7 @@ def main():
         elif parameter == 'rw':
             data = currency_now(api_key, 'EUR', 'RUB')
 
-            if (data is not None):
+            if data is not None:
                 save_to_json_cash(data)
             else:
                 print(f"Данные с сайта получить не удалось")
@@ -97,10 +105,9 @@ def main():
         elif parameter == 'l':
             data = currency_list(api_key)
         elif parameter == 't':
-            data = currency_list(api_key)
+            telebot_in_work()
         else:
             print(f"неверный параметр")
-
 
         for key, value in data.items():
             if type(value) is dict:
@@ -109,8 +116,6 @@ def main():
                     print(f"  {key}: {value}")
             else:
                 print(key, value, sep=': ')
-
-
 
 
 if __name__ == '__main__':
---
+++ src/tele_bot.py
@@ -0,0 +1,102 @@
+import telebot
+from src.apilayer_currency import currency_now
+from src.apilayer_currency import currency_list
+
+from src.apilayer_currency import save_to_json_cash
+from src.apilayer_currency import read_json_cash
+from decouple import config
+
+bot = telebot.TeleBot(config('TG_BOT_API_KEY'))
+
+
+@bot.message_handler(commands=['start', 'stop'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#     bot.reply_to(message, message.text)
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    commands_list = """
+    /apilayer - Получить данные с сайта, в файл не писать
+/apilayer_to_file - Получить данные с сайта и записать в файл
+/file - Получить данные или из файла
+/list - Получить список валют с сайта
+    """
+
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, commands_list)
+    elif message.text == "/file": # ['/apilayer', '/apilayer_to_file', '/file', '/list']:
+        data: dict = {}
+        data = read_json_cash()
+        data_in_bot = ''
+
+        for key, value in data.items():
+            if type(value) is dict:
+                data_in_bot += f'{key}:\n'
+                for key2, value2 in value.items():
+                    data_in_bot += f"  {key2}: {value2}\n"
+            else:
+                data_in_bot += f'{key}:  {value}\n'
+
+        # print(telebot_in_work(str(message.text)))
+        bot.send_message(message.from_user.id, '!!!!!!!!!!!!!!!!!!')
+        bot.send_message(message.from_user.id, str(data_in_bot))
+    elif message.text == "/list":
+        bot.send_message(message.from_user.id, telebot_in_work("/list"))
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+    print(f"I do...")
+
+
+@bot.message_handler(commands=['apilayer', 'apilayer_to_file', 'file', 'list'])
+def send_currency(message):
+    bot.reply_to(message, telebot_in_work(message.text))
+
+
+
+
+@bot.message_handler(commands=['apilayer', 'apilayer_to_file', 'file', 'list'])
+def telebot_in_work(parameter):
+    api_key = config('TG_BOT_API_KEY')
+    print(api_key)
+
+    api_key = config('APILAYER_APIKEY')
+    print(api_key)
+
+    data: dict = {}
+
+    if parameter == '/apilayer':
+        data = currency_now(api_key, 'EUR', 'RUB')
+    elif parameter == '/apilayer_to_file':
+        data = currency_now(api_key, 'EUR', 'RUB')
+
+        if data is not None:
+            save_to_json_cash(data)
+        else:
+            print(f"Данные с сайта получить не удалось")
+    elif parameter == '/file':
+        data = read_json_cash()
+    elif parameter == '/list':
+        data = currency_list(api_key)
+    else:
+        print(f"неверный параметр")
+
+    data_in_bot = ''
+
+    for key, value in data.items():
+        if type(value) is dict:
+            data_in_bot += f'{key}:\n'
+            for key2, value2 in value.items():
+                data_in_bot += f"  {key2}: {value2}\n"
+        else:
+            data_in_bot += f'{key}:  {value}\n'
+    return data_in_bot
+
+
+bot.infinity_polling()
